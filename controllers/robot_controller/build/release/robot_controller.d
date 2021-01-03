955482e327455ad34e738682c244dc9c3abaff99
ozcannn14 <furkan_bf14@hotmail.com>
2021-01-03 23:34:20 +0300

    remove build folder

--- controllers/robot_controller/build/release/robot_controller.d
+++
@@ -1,9 +0,0 @@
-build/release/robot_controller.o: robot_controller.cpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- ground_robot.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- D:/Program\ Files/Webots/include/controller/c/webots/types.h \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Receiver.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/utils/AnsiCodes.hpp
